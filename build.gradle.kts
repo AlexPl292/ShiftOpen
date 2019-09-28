c6fee5f63621d5f5610ec6b69eb056449d037666
Alex Plate <aleksei.plate@jetbrains.com>
2019-09-29 01:01:03 +0300

    Update build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,27 +1,15 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     id("org.jetbrains.intellij") version "0.4.10"
     kotlin("jvm") version "1.3.50"
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+group = "dev.feedforward"
+version = "1.0"
 
 repositories {
     mavenCentral()
 }
 
-dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-}
-
 intellij {
     version = "2019.2"
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
 }
