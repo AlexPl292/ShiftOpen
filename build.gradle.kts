f13abc3e985ba806e2da7cd24dc2ac8e96f4ac75
Alex Plate <aleksei.plate@jetbrains.com>
2019-09-29 13:08:26 +0300

    Configure kotlin in project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     id("org.jetbrains.intellij") version "0.4.10"
     kotlin("jvm") version "1.3.50"
@@ -16,4 +18,17 @@ intellij {
     updateSinceUntilBuild = false
     downloadSources = true
     instrumentCode = true
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "1.8"
 }
